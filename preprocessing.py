a3e2b379654c9d79dba23d2c2a7e17f40864fe8b
Eduardo Emery <emeryecs@gmail.com>
2017-03-02 13:51:39 -0300

    Adds targetID

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,15 @@
 
 import pandas as pd
 
-df = pd.read_csv("Data/gas_stations_database--fixed_date.csv")
+# Reads the data from the .csv file
+columns_names = ["CITY", "GAS_STATION", "ID", "FUEL_TYPE", "COST", "DAY", "MONTH", "YEAR"]
+df = pd.read_csv("Data/gas_stations_database--fixed_date.csv", names=columns_names)
+
 
 # Clusters by individuals and sorts by timestamp
-print df.sort_values(['ID', 'YEAR', 'MONTH', 'DAY'], ascending=False)
+#print df.sort_values(['ID', 'YEAR', 'MONTH', 'DAY'], ascending=False)
+
+
+# Chooses one specific ID
+targetID = df['ID'] == '3649HAR'
+print df[targetID].sort_values(['YEAR', 'MONTH', 'DAY'], ascending=False)
