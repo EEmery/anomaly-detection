b0d28d9ca09e50aa822c47bf5b46fbbe66f45bb8
Eduardo Emery <emeryecs@gmail.com>
2017-03-21 23:09:27 -0300

    Adds independent amount columns for each fuel

--- preprocessing.py
+++ preprocessing.py
@@ -36,31 +36,46 @@ if f_normalize:
 	df['AMOUNT'] = df['AMOUNT']/max_amount
 
 
-# Maps fuel types to numbers
-df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASOLINAESPECIAL'] = 1
-df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASNATURALVEHICULAR'] = 2
-df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASOLINAPREMIUM'] = 3
-df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'DIESELOIL'] = 4
+
 
 # Creates a quarter column
 df['QUARTER'] = 0
-df['QUARTER'].loc[df['MONTH'] == 1] = df['QUARTER'].loc[df['MONTH'] == 2] = df['QUARTER'].loc[df['MONTH'] == 3] = 1
-df['QUARTER'].loc[df['MONTH'] == 4] = df['QUARTER'].loc[df['MONTH'] == 5] = df['QUARTER'].loc[df['MONTH'] == 6] = 2
-df['QUARTER'].loc[df['MONTH'] == 7] = df['QUARTER'].loc[df['MONTH'] == 8] = df['QUARTER'].loc[df['MONTH'] == 9] = 3
-df['QUARTER'].loc[df['MONTH'] == 10] = df['QUARTER'].loc[df['MONTH'] == 11] = df['QUARTER'].loc[df['MONTH'] == 12] = 4
+df['QUARTER'].loc[(df['MONTH'] >= 1) & (df['MONTH'] <= 3)] = 1
+df['QUARTER'].loc[(df['MONTH'] >= 4) & (df['MONTH'] <= 6)] = 2
+df['QUARTER'].loc[(df['MONTH'] >= 7) & (df['MONTH'] <= 9)] = 3
+df['QUARTER'].loc[(df['MONTH'] >= 10) & (df['MONTH'] <= 12)] = 4
+
+
+# Maps fuel types to numbers
+#df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASOLINAESPECIAL'] = 1
+#df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASNATURALVEHICULAR'] = 2
+#df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'GASOLINAPREMIUM'] = 3
+#df['FUEL_TYPE'].loc[df['FUEL_TYPE'] == 'DIESELOIL'] = 4
+
+
+# Creates a column for each fuel
+df['GE'] = df['AMOUNT'].loc[df['FUEL_TYPE'] == 'GASOLINAESPECIAL']
+df['GNV'] = df['AMOUNT'].loc[df['FUEL_TYPE'] == 'GASNATURALVEHICULAR']
+df['GP'] = df['AMOUNT'].loc[df['FUEL_TYPE'] == 'GASOLINAPREMIUM']
+df['DO'] = df['AMOUNT'].loc[df['FUEL_TYPE'] == 'DIESELOIL']
 
 # Creates a frequency column
 df['FREQUENCY'] = 1
 
 
+################################### MONTHLY GROUPING ####################################
+
 # Groups ID's fuel consuption by month
-monthly_grouped = df.groupby(['ID', 'YEAR', 'MONTH', 'FUEL_TYPE'], as_index=False).sum()
+monthly_grouped = df.groupby(['ID', 'YEAR', 'MONTH'], as_index=False).sum()
+
+# Calculates the mean of the first half of the month
+#monthly_grouped['HALF_1'] = df['AMOUNT'].loc(df)
 
 # Calculates monthly mean
-monthly_grouped['MEAN'] = df.groupby(['ID', 'YEAR', 'MONTH', 'FUEL_TYPE'], as_index=False).mean()['AMOUNT']
+monthly_grouped['MEAN'] = df.groupby(['ID', 'YEAR', 'MONTH'], as_index=False).mean()['AMOUNT']
 
 # Calculates monthly standard deviation
-monthly_grouped['STD'] = df.groupby(['ID', 'YEAR', 'MONTH', 'FUEL_TYPE']).std().reset_index(0).reset_index(drop=True)['AMOUNT']
+monthly_grouped['STD'] = df.groupby(['ID', 'YEAR', 'MONTH']).std().reset_index(0).reset_index(drop=True)['AMOUNT']
 
 # Cleans final dataframe
 del monthly_grouped['DAY']
@@ -70,14 +85,16 @@ del monthly_grouped['QUARTER']
 monthly_grouped = monthly_grouped.sort_values(['ID', 'YEAR', 'MONTH'], ascending=False)
 
 
+################################## QUARTERLY GROUPING ###################################
+
 # Groups ID's fuel consuption by quarter
-quarterly_grouped = df.groupby(['ID', 'YEAR', 'QUARTER', 'FUEL_TYPE'], as_index=False).sum()
+quarterly_grouped = df.groupby(['ID', 'YEAR', 'QUARTER'], as_index=False).sum()
 
 # Calculates quarterly mean
-quarterly_grouped['MEAN'] = df.groupby(['ID', 'YEAR', 'QUARTER', 'FUEL_TYPE'], as_index=False).mean()['AMOUNT']
+quarterly_grouped['MEAN'] = df.groupby(['ID', 'YEAR', 'QUARTER'], as_index=False).mean()['AMOUNT']
 
 # Calculates quarterly standard deviation
-quarterly_grouped['STD'] = df.groupby(['ID', 'YEAR', 'QUARTER', 'FUEL_TYPE']).std().reset_index(0).reset_index(drop=True)['AMOUNT']
+quarterly_grouped['STD'] = df.groupby(['ID', 'YEAR', 'QUARTER']).std().reset_index(0).reset_index(drop=True)['AMOUNT']
 
 # Cleans final dataframe
 del quarterly_grouped['DAY']
@@ -87,6 +104,8 @@ del quarterly_grouped['MONTH']
 quarterly_grouped = quarterly_grouped.sort_values(['ID', 'YEAR', 'QUARTER'], ascending=False)
 
 
+
+
 # Creates output folder path if it does not exists
 if not exists(output_file_path):
 	makedirs(output_file_path)
@@ -94,7 +113,10 @@ if not exists(output_file_path):
 file_end = "-normalized.csv" if f_normalize else ".csv"
 
 # Saves both generated dataframes into .csv files
-monthly_grouped.to_csv(output_file_path+"monthly_grouped"+file_end, index=False, header=True)
-quarterly_grouped.to_csv(output_file_path+"quartely_grouped"+file_end, index=False, header=True)
+#monthly_grouped.to_csv(output_file_path+"monthly_grouped"+file_end, index=False, header=True)
+#quarterly_grouped.to_csv(output_file_path+"quartely_grouped"+file_end, index=False, header=True)
+
+#print monthly_grouped.head(30)
+print df.head(10)
 
 print "\nSuccessfully finished!"
