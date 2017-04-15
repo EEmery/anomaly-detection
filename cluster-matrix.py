324dd5be8d10747f0063c36d6bf91acc3cb3b0f4
Eduardo Emery <emeryecs@gmail.com>
2017-04-15 06:19:08 -0300

    Generate final result

--- cluster-matrix.py
+++ cluster-matrix.py
@@ -3,6 +3,7 @@ print "Importing packages\n"
 import pandas as pd
 import matplotlib.pyplot as plt
 from warnings import filterwarnings
+from numpy import nan
 
 
 # Ignores warnings
@@ -17,6 +18,8 @@ file_names = ['weekly', 'monthly', 'quarterly', 'semesterly']
 
 # Iterates over all periods
 for period, period_amount, file_name in zip(periods, periods_amounts, file_names):
+
+	print "Making " + file_name + " analysis"
 	
 	# Opens file (related to the period)
 	cluster_matrix = pd.read_csv(input_file_path + file_name + '_analysis.csv')
@@ -31,7 +34,29 @@ for period, period_amount, file_name in zip(periods, periods_amounts, file_names
 	cluster_matrix = cluster_matrix.set_index(['ID', period])
 	cluster_matrix = cluster_matrix.stack().unstack(1)
 
+	
+	# Gets the period of higher frequency
+	max_frequencies = cluster_matrix.loc(axis=0)[:, 'FREQUENCY'].idxmax(axis=1).reset_index().rename(columns={0:'STAMP'})
+
+	# Creates a data frame for final results
+	result = pd.DataFrame(columns = ['ID', 'GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN'])
+
+	# Iterates over the ID's
+	for i, ID, STAMP in zip(range(len(max_frequencies)), max_frequencies['ID'], max_frequencies['STAMP']):
+		data = cluster_matrix.loc(axis=0)[ID].ix[1:, STAMP]						# Gets the means from the higher frequency period
+		
+		row = [ID]
+		for mean in ['GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN']:				# Iterates over all fule type means
+			try:
+				row.append(data[mean])											# Appends to final result
+			except KeyError:
+				row.append(nan)
+
+		result.loc[i] = row														# Appends to result data frame
+
+
 	# Show some data
 	if period == 'MONTH':
+		print len(result)
 		cluster_matrix.loc['741NKH'].T.plot.bar()
 		plt.show()
