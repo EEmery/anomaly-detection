2102ce926a3a674b32a01c1a1f4ee90b3e7e5bb5
Eduardo Emery <emeryecs@gmail.com>
2017-04-19 13:29:51 -0300

    Change name from "cluster-matrix.py" to "max-frequency.py"

--- max-frequency.py
+++ max-frequency.py
@@ -22,28 +22,28 @@ for period, period_amount, file_name in zip(periods, periods_amounts, file_names
 	print "Making " + file_name + " analysis"
 	
 	# Opens file (related to the period)
-	cluster_matrix = pd.read_csv(input_file_path + file_name + '_analysis.csv')
+	periodic_analysis = pd.read_csv(input_file_path + file_name + '_analysis.csv')
 
 	# Remover YEAR necessity by increasing period limits
-	cluster_matrix[period] = cluster_matrix[period] + (cluster_matrix['YEAR'] - 2015) * period_amount
+	periodic_analysis[period] = periodic_analysis[period] + (periodic_analysis['YEAR'] - 2015) * period_amount
 
 	# Slices data frame to get only necessary columns
-	cluster_matrix = cluster_matrix[['ID', period, 'FREQUENCY', 'GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN']]
+	periodic_analysis = periodic_analysis[['ID', period, 'FREQUENCY', 'GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN']]
 
 	# Reshapes data frame to desired shape
-	cluster_matrix = cluster_matrix.set_index(['ID', period])
-	cluster_matrix = cluster_matrix.stack().unstack(1)
+	periodic_analysis = periodic_analysis.set_index(['ID', period])
+	periodic_analysis = periodic_analysis.stack().unstack(1)
 
 	
 	# Gets the period of higher frequency
-	max_frequencies = cluster_matrix.loc(axis=0)[:, 'FREQUENCY'].idxmax(axis=1).reset_index().rename(columns={0:'STAMP'})
+	max_frequencies = periodic_analysis.loc(axis=0)[:, 'FREQUENCY'].idxmax(axis=1).reset_index().rename(columns={0:'STAMP'})
 
 	# Creates a data frame for final results
-	result = pd.DataFrame(columns = ['ID', 'GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN'])
+	result_df = pd.DataFrame(columns = ['ID', 'GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN'])
 
 	# Iterates over the ID's
 	for i, ID, STAMP in zip(range(len(max_frequencies)), max_frequencies['ID'], max_frequencies['STAMP']):
-		data = cluster_matrix.loc(axis=0)[ID].ix[1:, STAMP]						# Gets the means from the higher frequency period
+		data = periodic_analysis.loc(axis=0)[ID].ix[1:, STAMP]					# Gets the means from the higher frequency period
 		
 		row = [ID]
 		for mean in ['GE_MEAN', 'GNV_MEAN', 'GP_MEAN', 'DO_MEAN']:				# Iterates over all fule type means
@@ -52,11 +52,11 @@ for period, period_amount, file_name in zip(periods, periods_amounts, file_names
 			except KeyError:
 				row.append(nan)
 
-		result.loc[i] = row														# Appends to result data frame
+		result_df.loc[i] = row													# Appends to result data frame
 
 
 	# Show some data
 	if period == 'MONTH':
-		print len(result)
-		cluster_matrix.loc['741NKH'].T.plot.bar()
+		print len(result_df)
+		periodic_analysis.loc['741NKH'].T.plot.bar()
 		plt.show()
